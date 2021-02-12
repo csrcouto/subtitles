6e33d26d119da1763fa77bb03873b1c441c22011
Cesar Couto <csrcouto@gmail.com>
2021-02-11 21:55:03 -0300

    agrupando as palavras

--- index.js
+++ index.js
@@ -5,19 +5,31 @@ const caminho = path.join(__dirname, "legendas");
 
 const simbolos = [
     '.', '?', '-', '\,', '"', '♪', '_', '\r', '<i>', '</i>',
-    '[', ']', '(', ')', '\'', '\-', '\.', '\'', ',', '\.',
-    '\"', '!'
+    '[', ']', '(', ')', '\'', '\-', '\.', '\'', ',', '\.', '\"', '!'
 ];
+
+function agruparPalavras(array) {
+    return array.reduce((acc, palavra) => {
+        palavra = palavra.toLowerCase();
+        if (acc[palavra]) {
+            acc[palavra] += 1;
+        } else {
+            acc[palavra] = 1;
+        };
+        return acc;
+    }, {});
+}
 
 fn.lerDiretorio(caminho)
     .then((arquivos) => (fn.filtrarExtencao(arquivos, ".srt")))
     .then((arquivosFiltrados) => (fn.lerArquivos(arquivosFiltrados)))
     .then((conteudoAgregado) => (conteudoAgregado.join("\n")))
     .then((conteudoTratado) => (conteudoTratado.split("\n")))
-    .then((linhas) => (fn.removerVazios(linhas)))
-    .then((linhas) => (fn.removerPadroes(linhas, "-->")))
-    .then((linhas) => (fn.removerNumeracao(linhas)))
-    .then((linhas) => (fn.removerSimbolos(linhas, simbolos)))
-    .then((linhas) => (fn.separarPalavras(linhas)))
+    .then((conteudo) => (fn.removerVazios(conteudo)))
+    .then((conteudo) => (fn.removerPadroes(conteudo, "-->")))
+    .then((conteudo) => (fn.removerNumeracao(conteudo)))
+    .then((conteudo) => (fn.removerSimbolos(conteudo, simbolos)))
+    .then((conteudo) => (fn.separarPalavras(conteudo)))
+    .then((conteudo) => (agruparPalavras(conteudo)))
     .then(console.log)
 ;
